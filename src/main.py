8acfb61d7224595f2828755a5bb3ead25b60ce6b
Can202 <mgoopazo@gmail.com>
2023-08-15 17:46:15 -0400

    separate gameLogic from Game init

--- src/main.py
+++ src/main.py
@@ -12,11 +12,76 @@ class Game:
     def __init__(self) -> None:
 
         self.window = pygame.display.set_mode((constant.DEFINEWIDTH, constant.DEFINEHEIGHT), pygame.FULLSCREEN)
-        self.screen = pygame.Surface((constant.WIDTH, constant.HEIGHT))
+        
         pygame.display.set_caption("Game")
 
         self.clock = pygame.time.Clock()
         self.running = True
+
+        self.mouseposX = 0
+        self.mouseposY = 0
+
+        self.deltaTime = 0
+
+        self.xoffset = 0
+        self.offset = pygame.Vector2(0, 0)
+        self.fix = 1
+        self.mousepressed = False
+
+        self.mainGame = GameLogic()
+
+
+
+    def mainloop(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.FINGERDOWN:
+                    self.mousepressed = True
+
+                    if event.type == pygame.FINGERDOWN:
+                        self.mouseposX = event.x * constant.WIDTH
+                        self.mouseposY = event.y * constant.HEIGHT
+                    else:
+                        self.mouseposX, self.mouseposY = (0, 0)
+                else:
+                    self.mousepressed = False
+            self.keys = pygame.key.get_pressed()
+
+
+            self.mainGame.mainloop(self.fix, self.offset,
+                                   self.deltaTime,
+                                   self.mouseposX, self.mouseposY,
+                                   self.mousepressed)
+
+            self.screenfix()
+            self.deltaTime = self.clock.tick(60) / 1000.0
+
+
+            self.window.fill((0, 0, 0))
+            self.window.blit(pygame.transform.scale(
+                self.mainGame.screen, (int(constant.WIDTH*self.fix), int(constant.HEIGHT*self.fix))), 
+                self.offset)
+
+            pygame.display.update()
+    
+    def screenfix(self):
+        height = self.window.get_height()
+        width = self.window.get_width()
+        if (height / 9) <= (width/16):
+            self.fix = (height / constant.HEIGHT)
+            self.offset.x = (width - (constant.WIDTH * self.fix)) / 2
+            self.offset.y = 0
+        else:
+            self.fix = (width / constant.WIDTH)
+            self.offset.x = 0
+            self.offset.y = (height - (constant.HEIGHT * self.fix)) / 2
+
+class GameLogic:
+    def __init__(self) -> None:
+        
+        self.screen = pygame.Surface((constant.WIDTH, constant.HEIGHT))
 
         self.clockOnScreen = objects.Node(pygame.Vector2((constant.WIDTH - 540)/2, 20))
         self.minuteHand = objects.Hand(pygame.Vector2((constant.WIDTH + 10)/2, (constant.HEIGHT + 120)/2), image.MINUTE, 10, 90)
@@ -27,10 +92,8 @@ class Game:
         self.btn2 = objects.Button(pygame.Vector2(950,200), _text="As2")
         self.btn3 = objects.Button(pygame.Vector2(20,500), _text="As3")
         self.btn4 = objects.Button(pygame.Vector2(950,500), _text="As4")
-        self.mouseposX = 0
-        self.mouseposY = 0
+        
 
-        self.language = "es"
 
 
         self.shuffle = True
@@ -41,12 +104,14 @@ class Game:
         self.otherbtn2 = objects.Hour() 
         self.otherbtn3 = objects.Hour() 
 
-        self.esbtn = objects.Button(pygame.Vector2(5,5),
+        self.language = "es"
+        self.esbtn = objects.Button(pygame.Vector2(35,65),
                                     image.ES, "",
                                     image.ES, image.ES)
-        self.enbtn = objects.Button(pygame.Vector2(100,5),
+        self.enbtn = objects.Button(pygame.Vector2(130,65),
                                     image.EN, "",
                                     image.EN, image.EN)
+        
 
         self.goods = 0
         self.goodstext = objects.Text(str(self.goods),
@@ -64,34 +129,19 @@ class Game:
             image.ERROR)
         self.erroranimation = False
 
+    def mainloop(self, _fix, _offset, _dt, _mpx, _mpy, _mp):
 
-        self.deltaTime = 0
+        self.fix = _fix
+        self.offset = _offset
+        self.deltaTime = _dt
+        self.mouseposX = _mpx
+        self.mouseposY = _mpy
 
-        self.xoffset = 0
-        self.offset = pygame.Vector2(0, 0)
-        self.fix = 1
-        self.mousepressed = False
-    def mainloop(self):
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.FINGERDOWN:
-                    self.mousepressed = True
+        self.mousepressed = _mp 
 
-                    if event.type == pygame.FINGERDOWN:
-                        self.mouseposX = event.x * constant.WIDTH
-                        self.mouseposY = event.y * constant.HEIGHT
-                    else:
-                        self.mouseposX, self.mouseposY = (0, 0)
-                else:
-                    self.mousepressed = False
-            self.keys = pygame.key.get_pressed()
-            self.update()
-            self.draw()
-
-            self.deltaTime = self.clock.tick(60) / 1000.0
-            pygame.display.update()
+        self.update()
+        self.draw()
+    
     def update(self):
         if self.shuffle:
             self.correctbtnnumber = random.randint(1,4)
@@ -219,18 +269,10 @@ class Game:
             self.erroronScreen.position.y = constant.HEIGHT
             self.erroranimation = False
 
-            
-            
-        
 
-
-        self.screenfix()
         self.timegood.update(self.deltaTime)
 
-
-
     def draw(self):
-        self.window.fill((0, 0, 0))
         self.background.draw(self.screen)
         self.clockOnScreen.draw(self.screen)
         self.minuteHand.draw(self.screen)
@@ -247,10 +289,6 @@ class Game:
         self.esbtn.draw(self.screen)
         self.enbtn.draw(self.screen)
 
-        self.window.blit(pygame.transform.scale(
-            self.screen, (int(constant.WIDTH*self.fix), int(constant.HEIGHT*self.fix))), 
-            self.offset)
-
     def return_angle_by_hour(self, hour, minutes):
         
         self.hourHand.rotation = 360 - ((30 * hour) + (minutes * (30 / 60)))
@@ -261,20 +299,6 @@ class Game:
         if minutes == 0:
             minutes = 60
         self.minuteHand.rotation = 360 - (30 * (minutes / 5))
-
-    
-    def screenfix(self):
-        height = self.window.get_height()
-        width = self.window.get_width()
-        if (height / 9) <= (width/16):
-            self.fix = (height / constant.HEIGHT)
-            self.offset.x = (width - (constant.WIDTH * self.fix)) / 2
-            self.offset.y = 0
-        else:
-            self.fix = (width / constant.WIDTH)
-            self.offset.x = 0
-            self.offset.y = (height - (constant.HEIGHT * self.fix)) / 2
-
         
 
 if __name__ == "__main__":
