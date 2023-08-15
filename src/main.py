6e0bf448e158379db82cdef23998b2b611ca9656
Can202 <mgoopazo@gmail.com>
2023-08-14 23:10:59 -0400

    fix animation v2

--- src/main.py
+++ src/main.py
@@ -193,15 +193,15 @@ class Game:
 
         if self.ticketanimation:
             if self.timegood.time < .5:
-                self.ticketonScreen.position.y -= (constant.HEIGHT + image.TICKET.get_height()/self.fix) * self.deltaTime
+                self.ticketonScreen.position.y -= (constant.HEIGHT + 252) * self.deltaTime
             if self.timegood.time > 1.5:
-                self.ticketonScreen.position.y += (constant.HEIGHT + image.TICKET.get_height()/self.fix) * self.deltaTime
+                self.ticketonScreen.position.y += (constant.HEIGHT + 252) * self.deltaTime
         
         if self.erroranimation:
             if self.timegood.time < .5:
-                self.erroronScreen.position.y -= (constant.HEIGHT + image.ERROR.get_height()) * self.deltaTime
+                self.erroronScreen.position.y -= (constant.HEIGHT + 290) * self.deltaTime
             if self.timegood.time > 1.5:
-                self.erroronScreen.position.y += (constant.HEIGHT + image.ERROR.get_height()) * self.deltaTime
+                self.erroronScreen.position.y += (constant.HEIGHT + 290) * self.deltaTime
         if self.timegood.timing == False:
             self.ticketonScreen.position.y = constant.HEIGHT
             self.ticketanimation = False
